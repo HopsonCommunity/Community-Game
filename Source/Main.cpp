032294a4974abe4338063ff7aee1a572dd1d2e99
KiddieD <djsndcx@gmail.com>
2017-03-29 19:30:11 +0200

    Added an intro
    Added an intro

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -3,7 +3,9 @@
 
 int main()
 {
-    std::cout << "Lets go..." << std::endl;
+    std::cout << "Hello there, coders!" << std::endl;
+    std::cout << "This is the community project" << std::endl;
+    std::cout << "Don't forget that you can always ask others about their \nintents on the Discord server!" << std::endl;
 
     return EXIT_SUCCESS;
 }
