8dac4abcd1734d1192b08994b4404ae07a904655
Your Name <mhopson@hotmail.co.uk>
2017-03-31 21:43:12 +0100

    Fix resource managers

--- src/resources/Resource_Manager.h
+++ src/resources/Resource_Manager.h
@@ -6,14 +6,21 @@ template <typename Res>
 class Resource_Manager
 {
     public:
+        Resource_Manager(std::string&& path, std::string&& extension)
+        :   m_path      (std::move(path))
+        ,   m_extension (std::move(extension))
+        {}
+
         const Res& get(const std::string& name)
         {
-            if (m_resourceMap.find())
+            std::string full = m_path + name + m_extension;
+
+            if (m_resourceMap.find(full))
             {
                 ///@TODO Throw error on fail?
                 add(name);
             }
-            return qGet(name);
+            return qGet(full);
         }
 
         const Res& qGet(const std::string& name)
@@ -30,5 +37,8 @@ class Resource_Manager
         }
 
     private:
+        std::string m_path;
+        std::string m_extension;
+
         std::unordered_map<std::string, Res> m_resourceMap;
 };
