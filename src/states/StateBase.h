c2647f445650b6a30ab24596e27f2a501fc9a017
0Zix0 <zixerino@gmail.com>
2017-04-03 00:50:35 +1000

    Made application protected.

--- src/states/StateBase.h
+++ src/states/StateBase.h
@@ -28,7 +28,7 @@ namespace State
             virtual void update (float dt) = 0;
             virtual void render (sf::RenderWindow& window) = 0;
 
-        private:
+        protected:
             Application* m_pApplication;
     };
 }
