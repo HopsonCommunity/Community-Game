c8e91650b79ddfed6367f083518c1fdd08159ae1
Matthew Hopson <Hopson97@users.noreply.github.com>
2017-04-02 09:20:15 +0100

    Update FileReader.h

--- src/util/FileReader.h
+++ src/util/FileReader.h
@@ -2,29 +2,24 @@
 
 #include <cstring>
 #include <iostream>
+#include <sstream>
 
 namespace Util
 {
-	std::string ReadFile(const std::string& filepath)
+	std::string readFile(const std::string& filepath)
 	{
-		FILE* file = fopen(filepath.c_str(), "rb");
-		if (file == nullptr)
+		std::ifstream inFile (filepath);
+        	std::string source;
+		std::stringstream stringStream;
+		
+		if (!inFile.is_open()) 
 		{
 			std::cout << "[FileReader.h] Couldn't open file \"" << filepath.c_str() << "\"!" << std::endl;
 			return "";
 		}
-
-		fseek(file, 0, SEEK_END);
-
-		unsigned long length = ftell(file);
-		char* data = new char[length + 1];
-		memset(data, 0, length + 1);
-		fseek(file, 0, SEEK_SET);
-		fread(data, 1, length, file);
-		fclose(file);
-
-		std::string result(data);
-		delete[] data;
-		return result;
+		
+		stringStream << inFile.rdbuf();
+        	source = stringStream.str();
+		return source;
 	}
 }
