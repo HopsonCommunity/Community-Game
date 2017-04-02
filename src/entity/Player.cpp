4f010cc52fcad131fe0353e92880652d20180ba2
Repertoi-e <dsotirov@windowslive.com>
2017-04-02 23:22:52 +0300

    Player animation

--- src/entity/Player.cpp
+++ src/entity/Player.cpp
@@ -1,11 +1,17 @@
 #include "Player.h"
 
+#include "../Application.h"
+
 namespace Framework
 {
 	Player::Player()
 	: Entity(100, 100)
 	, m_moveState(IDLE)
+	, m_textureAtlas()
+	, m_currentFrame(new sf::Texture())
 	{
+		m_textureAtlas.loadFromFile("res/textures/player_modelDefault.png");
+
 		constexpr static auto MAX_HEALTH = 800;
 
 		addEffect(std::make_unique<HealthBoost>(DURATION_INFINITE, MAX_HEALTH, 0));
@@ -14,23 +20,62 @@ namespace Framework
 		addEffect(std::make_unique<Defense>(20 * 5, 20, 30));
 	}
 
-	void Player::input()
+	Player::~Player()
 	{
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-			velocity.x += 0.1;
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-			velocity.x -= 0.1;
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-			velocity.y -= 0.1;
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-			velocity.y += 0.1;
+		delete m_currentFrame;
 	}
 
-	void Player::update()
+	float g_movement_speed = 2;
+
+	void Player::input(Application* app)
+	{
+		m_moveState = IDLE;
+
+		if (app->inputPressed(MOVE_UP))
+		{
+			m_moveState = WALK;
+			velocity.y -= g_movement_speed;
+		}
+		if (app->inputPressed(MOVE_DOWN))
+		{
+			m_moveState = WALK;
+			velocity.y += g_movement_speed;
+		}
+		if (app->inputPressed(MOVE_LEFT))
+		{
+			m_faceDir = FaceDirection::LEFT;
+			m_moveState = WALK;
+			velocity.x -= g_movement_speed;
+		}
+		if (app->inputPressed(MOVE_RIGHT))
+		{
+			m_faceDir = FaceDirection::RIGHT;
+			m_moveState = WALK;
+			velocity.x += g_movement_speed;
+		}
+	}
+
+	void Player::update(float dt)
 	{
 		Entity::update();
 
+		m_frameCounter++;
+		if (m_frameCounter > 5)
+		{
+			m_frame++;
+			m_frameCounter = 0;
+		}
+		if (m_frame > 7)
+			m_frame = 0;
 
+		sf::IntRect sprite_area(m_frame * 32, m_moveState == WALK ? 33 : 0, 32, 32);
+		m_currentFrame->loadFromImage(m_textureAtlas, sprite_area);
+		getShape().setTexture(m_currentFrame);
+		getShape().setTextureRect(m_faceDir == FaceDirection::RIGHT ? sf::IntRect(0, 0, 32, 32) : sf::IntRect(32, 0, -32, 32));
+
+		getShape().move(velocity);
+
+		velocity *= 0.5f;
 	}
 
 	void Player::applyDamage(const Damage& dmg)
--- src/entity/Player.h
+++ src/entity/Player.h
@@ -2,23 +2,38 @@
 
 #include "Entity.h"
 
+class Application;
+
 namespace Framework
 {
 	class Player : public Entity 
 	{
 	public:
+		enum FaceDirection : byte
+		{
+			RIGHT, LEFT
+		};
+
 		enum MoveState : byte
 		{
 			IDLE, WALK
 		};
 	private:
+		sf::Image m_textureAtlas;
+		sf::Texture* m_currentFrame;
+
 		MoveState m_moveState;
+		FaceDirection m_faceDir;
+
+		int m_frameCounter = 0;
+		int m_frame = 0;
 	public:
 		Player();
-		
+		~Player();
+
 		void applyDamage(const Damage& dmg) override;
 	
-		void input();
-		virtual void update();
+		void input(Application* app);
+		void update(float dt);
 	};
 }
