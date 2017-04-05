f7594ba65b1b392cdeeeeb75a2ac10536c6ad1fe
Your Name <mhopson@hotmail.co.uk>
2017-04-05 17:20:49 +0100

    fix code style in level.h

--- src/level/Level.h
+++ src/level/Level.h
@@ -11,29 +11,27 @@ namespace Framework
 
 namespace Level
 {
-
 	class Level
 	{
-	private:
-		unsigned int m_width;
-		unsigned int m_height;
-		std::vector<Tile::Tile*> m_tiles;
-		std::vector<Framework::Entity*> m_entities;
+        public:
+            Level() {}
+            Level(unsigned int width, unsigned int height);
 
-	public:
-		Level() {}
-		Level(unsigned int width, unsigned int height);
+            void addEntity(Framework::Entity* entity);
+            void setTile(unsigned int x, unsigned int y, Tile::Tile& tile);
+            inline Tile::Tile* getTile(unsigned int x, unsigned int y)
+            {
+                if (x < 0 || x >= m_width || y < 0 || y >= m_height) return nullptr;
+                return m_tiles[x + y * m_width];
+            }
 
-		void addEntity(Framework::Entity* entity);
-		void setTile(unsigned int x, unsigned int y, Tile::Tile& tile);
-		inline Tile::Tile* getTile(unsigned int x, unsigned int y)
-		{
-			if (x < 0 || x >= m_width || y < 0 || y >= m_height) return nullptr;
-			return m_tiles[x + y * m_width];
-		}
+            void update(const Timestep& ts);
+            void render(sf::RenderWindow& window);
 
-		void update(const Timestep& ts);
-		void render(sf::RenderWindow& window);
+        private:
+            unsigned int m_width;
+            unsigned int m_height;
+            std::vector<Tile::Tile*> m_tiles;
+            std::vector<Framework::Entity*> m_entities;
 	};
-
 }
