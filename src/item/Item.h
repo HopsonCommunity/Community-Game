cb8eedfeb41a97844a7e501708213766e1fdbac3
kaizi99 <kaizi99@gmx.de>
2017-04-13 13:50:06 +0200

    Basics for the ICS

---
+++ src/item/Item.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "../util/Types.h"
+#include <memory>
+#include <unordered_map>
+
+#include "Component.h"
+
+namespace Item
+{
+    class Item
+    {
+    public:
+        Item();
+
+        void addComponent(std::unique_ptr<Component> component);
+
+        template<typename T>
+        const T* getComponent() const
+        {
+            return getComponentInternal<T>().get();
+        }
+
+        template<typename T>
+        T* getComponent()
+        {
+            return (T*)getComponentInternal<T>();
+        }
+
+        uint64 getID() { return m_ID; }
+
+    private:
+        template <typename T>
+        const Component* getComponentInternal() const
+        {
+            ComponentType* type = T::getStaticType();
+            auto it = m_components.find(type);
+            if (it == m_components.end())
+                return nullptr;
+            return it->second.get();
+        }
+
+        uint64 m_ID;
+        std::unordered_map<ComponentType*, std::unique_ptr<Component>> m_components;
+    };
+}
---
+++ src/item/ItemFactory.cpp
@@ -0,0 +1,11 @@
+﻿#include "ItemFactory.h"
+
+namespace Item
+{
+	std::unique_ptr<Item> ItemFactory::createItem(const std::string& name)
+	{
+		auto item = std::make_unique<Item>();
+
+		return item;
+	}
+}
