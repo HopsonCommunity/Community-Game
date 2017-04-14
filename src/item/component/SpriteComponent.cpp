5f7dbeaaf381e8d120e1a326edf457d9cbffc2d2
Haker53535 <haker53535jakub@gmail.com>
2017-04-14 09:54:08 +0200

    Created SpriteComponent

---
+++ src/item/component/SpriteComponent.cpp
@@ -0,0 +1,19 @@
+#include "SpriteComponent.h"
+#include "../../Application.h"
+
+namespace Item {
+	SpriteComponent::SpriteComponent(nlohmann::json json)
+	: sprite(sf::Sprite(Application::instance->getResources().textures.get(json["src"]), sf::IntRect(json["rect"]["left"], json["rect"]["top"], json["rect"]["width"], json["rect"]["height"]))){
+
+	}
+	ComponentType* SpriteComponent::getStaticType()
+	{
+		static ComponentType type({ "Sprite" });
+		return &type;
+	}
+
+	ComponentType* SpriteComponent::getType() const
+	{
+		return getStaticType();
+	}
+}
