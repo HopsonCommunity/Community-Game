ae2447917c36a595c7f0070c5d5735f0ea49c228
defiantredpill <defiantredpill@gmail.com>
2017-04-14 17:53:55 -0400

    let the compiler do it if can (implicitly inline also)

--- src/maths/Maths.h
+++ src/maths/Maths.h
@@ -2,12 +2,12 @@
 
 constexpr float PI = 3.14159265358f;
 
-inline float toRadians(float degrees)
+constexpr float toRadians(float degrees)
 {
 	return (float)(degrees * (PI / 180.0f));
 }
 
-inline float toDegrees(float radians)
+constexpr float toDegrees(float radians)
 {
 	return (float)(radians * (180.0f / PI));
 }
