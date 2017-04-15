578bbf2f5f23baa5c66e19d5b3d95db66a01bf9a
Haker53535 <haker53535jakub@gmail.com>
2017-04-15 17:05:20 +0200

    Created inventory component for entities

---
+++ src/entity/component/InventoryComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by jakub on 15.04.17.
+//
+
+#ifndef COMMUNITY_GAME_INVENTORYCOMPONENT_H
+#define COMMUNITY_GAME_INVENTORYCOMPONENT_H
+
+
+
+class InventoryComponent {
+
+};
+
+
+
+#endif //COMMUNITY_GAME_INVENTORYCOMPONENT_H
