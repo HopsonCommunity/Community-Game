2f4fa0d02678b5c1bb009b282f4b1969c7fcd6ed
Repertoi-e <dsotirov@windowslive.com>
2017-04-15 17:22:29 +0300

    FileReader now ignores BOM when reading UTF-8 (with signature) files which are default saved from Visual Studio and cause crashes with JSON parsing (unexpected symbols) + fixed laggy Zombie AI

--- src/util/FileUtil.cpp
+++ src/util/FileUtil.cpp
@@ -1,18 +1,44 @@
-#include "FileUtil.h"
+﻿#include "FileUtil.h"
 
-#include <iostream>
+// Code is referenced from:
+// https://vicidi.wordpress.com/2015/03/09/reading-utf-file-with-bom-to-utf-8-encoded-stdstring-in-c11-on-windows/
+
 #include <fstream>
 #include <sstream>
 
+#define ENCODING_ASCII      0
+#define ENCODING_UTF8       1
+
 std::string getFileContents(const std::string& filePath)
 {
-    std::ifstream inFile(filePath);
-    if (!inFile.is_open())
-    {
-        std::cerr << "Unable to read " << filePath << "\n";
-    }
+	std::string result;
+	std::ifstream ifs(filePath.c_str(), std::ios::binary);
+	std::stringstream ss;
+	int encoding = ENCODING_ASCII;
 
-    std::stringstream stream;
-    stream << inFile.rdbuf();
-    return stream.str();
+	if (!ifs.is_open()) 
+	{
+		result.clear(); // Unable to read file
+		return result;
+	}
+	else if (ifs.eof())
+		result.clear();
+	else 
+	{
+		int ch1 = ifs.get();
+		int ch2 = ifs.get();
+		
+		int ch3 = ifs.get();
+		if (ch1 == 0xef && ch2 == 0xbb && ch3 == 0xbf) 
+			encoding = ENCODING_UTF8; // The file contains UTF-8 BOM
+		else 
+		{
+			encoding = ENCODING_ASCII; // The file does not have BOM
+			ifs.seekg(0);
+		}
+	}
+	ss << ifs.rdbuf() << '\0';
+	result = ss.str();
+
+	return result;
 }
