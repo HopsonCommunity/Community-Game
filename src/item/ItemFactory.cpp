73a3e7e53139ab8faa3c57cdfdeee58196708a6b
kaizi99 <kaizi99@gmx.de>
2017-04-16 16:49:44 +0200

    Revert "Changed item factory to work with JSON"
    This reverts commit 21f136cdaffbfdd83b0048c92f2b7c39c01ee6f4.

--- src/item/ItemFactory.cpp
+++ src/item/ItemFactory.cpp
@@ -1,22 +1,11 @@
 ﻿#include "ItemFactory.h"
-#include "../util/FileUtil.h"
-#include "../util/json.hpp"
 
 namespace Item
 {
-	std::unique_ptr<Item> ItemFactory::createItem(const std::string& filePath)
+	std::unique_ptr<Item> ItemFactory::createItem(const std::string& name)
 	{
-		std::string source = getFileContents("res/items/" + filePath + ".json");
-		nlohmann::json json = nlohmann::json::parse(source.c_str());
+		auto item = std::make_unique<Item>();
 
-		std::unique_ptr<Item> item = std::make_unique<Item>();
-
-		std::vector<nlohmann::json> componentsJSON = json["components"];
-		for (int i = 0; i < componentsJSON.size(); i++)
-		{
-			nlohmann::json componentJSON = componentsJSON[i];
-			///@TODO: All components "if's"
-		}
 		return item;
 	}
 }
