20634215655989df86bf3d95f6ac2909bb1a7c30
Marcel Guzik <mguzik555@gmail.com>
2017-05-05 02:05:11 +0200

    Temporary fix of #202 - removed zombie

--- src/app/states/StatePlaying.cpp
+++ src/app/states/StatePlaying.cpp
@@ -28,11 +28,6 @@ namespace State
 		m_level.addEntity(std::move(player));
 
 		m_level.player->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f, m_level.player_spawn.x * 32.0f };
-
-		std::unique_ptr<Entity::Entity> zombie = entityFactory->createEntity("enemy/Zombie.json");
-		zombie->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f + 170, m_level.player_spawn.x * 32.0f + 170 };
-
-		m_level.addEntity(std::move(zombie));
 	}
 
 	void Playing::event(sf::Event& event)
