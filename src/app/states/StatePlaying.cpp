b76cacb924647eb063063f6872469de6484e0a44
= <=>
2017-05-21 08:30:04 +0200

    Repaired player placement problem

--- src/app/states/StatePlaying.cpp
+++ src/app/states/StatePlaying.cpp
@@ -29,20 +29,20 @@ namespace State
 		m_level.player = player.get();
 		m_level.addEntity(std::move(player));
 
-		m_level.player->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f, m_level.player_spawn.x * 32.0f };
+		m_level.player->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f, m_level.player_spawn.y * 32.0f };
 
 		std::unique_ptr<Entity::Entity> lantern = entityFactory->createEntity("Lantern.json");
-		lantern->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f + 170, m_level.player_spawn.x * 32.0f + 170 };
+		lantern->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f + 170, m_level.player_spawn.y * 32.0f + 170 };
 
 		m_level.addEntity(std::move(lantern));
 
 		std::unique_ptr<Entity::Entity> lantern2 = entityFactory->createEntity("Lantern.json");
-		lantern2->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f + 140, m_level.player_spawn.x * 32.0f + 170 };
+		lantern2->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f + 140, m_level.player_spawn.y * 32.0f + 170 };
 
 		m_level.addEntity(std::move(lantern2));
 
 		std::unique_ptr<Entity::Entity> zombie = entityFactory->createEntity("enemy/Zombie.json");
-		zombie->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f + 140, m_level.player_spawn.x * 32.0f + 170 };
+		zombie->getComponent<Entity::PhysicsComponent>()->pos = { m_level.player_spawn.x * 32.0f + 140, m_level.player_spawn.y * 32.0f + 170 };
 
 		m_level.addEntity(std::move(zombie));
 	}
